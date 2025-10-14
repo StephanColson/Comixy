87ec6920b660556d20ab2cdeaa92b0a3b39c1355
StephanColson <151721516+StephanColson@users.noreply.github.com>
2025-10-14 12:57:19 +0200

    Fixed the title search

--- src/pages/ComicPage.jsx
+++ src/pages/ComicPage.jsx
@@ -1,7 +1,7 @@
 import {Section} from "../components/Section.jsx";
 import {Comics} from "../components/Comics.jsx";
 import {SectionCard} from "../components/SectionCard.jsx";
-import {Card, Col, Container} from "react-bootstrap";
+import {Card, Col, Container, Form} from "react-bootstrap";
 import {useState} from "react";
 
 function YearPublished (props) {
@@ -35,27 +35,6 @@ function YearsPublished (props){
     )
 }
 
-function FilterTitle (props) {
-    const {filterTitle, setFilterTitle} = props;
-
-    const handleChange = (e) => {
-        setFilterTitle(e.target.value);
-    };
-
-    return(
-        <div>
-            <label>Filter Title:</label>
-            <input
-                type="text"
-                className="form-control"
-                placeholder="Search title..."
-                value={filterTitle}
-                onChange={handleChange}
-            />
-        </div>
-    )
-}
-
 export function ComicPage(props) {
     const {comics, title} = props;
     const [filterTitle, setFilterTile] = useState("");
@@ -66,20 +45,16 @@ export function ComicPage(props) {
     return (
         <>
         <div className="d-flex justify-content-between">
-                <div className="bg-dark-subtle p-2">
-                    <div className="mb-2">
-                        <h2>Filters</h2>
-                    </div>
-
-                    <div>
-                        <FilterTitle filterTitle={filterTitle} setFilterTitle={setFilterTile}/>
-                    </div>
-                </div>
+                <Form className="bg-dark-subtle p-2">
+                    <Form.Label>Filters:</Form.Label>
+                    <Form.Control className="m-2" value={filterTitle}
+                                  onChange={e => setFilterTile(e.target.value)}/>
+                </Form>
 
                 <Section title="Comic List">
                     <Comics comics={filteredComics}/>
                 </Section>
-            </div>
+        </div>
 
             <Section title="Comics grouped by published year">
                 <YearsPublished comics={comics}/>
