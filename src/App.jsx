d5a4c6a1686ccefc9ed773289862239c67cffd92
StephanColson <151721516+StephanColson@users.noreply.github.com>
2025-10-29 14:45:21 +0100

    Firebase initialized

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import 'react-tabs/style/react-tabs.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'rc-pagination/assets/index.css';
+import './api/firebase.js';
 import {Tab, TabList, TabPanel, Tabs} from "react-tabs";
 import {ComicPage} from "./pages/ComicPage.jsx";
 import {HomePage} from "./pages/HomePage.jsx";
