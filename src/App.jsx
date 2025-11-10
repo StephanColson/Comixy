413b18b1af2a07cab620b7a5a70c2ab800a99b34
StephanColson <151721516+StephanColson@users.noreply.github.com>
2025-11-10 17:06:43 +0100

    Changed Navigation style from tabs to navbar.

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,73 @@ import 'react-tabs/style/react-tabs.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'rc-pagination/assets/index.css';
 import './api/firebase.js';
-import {Tab, TabList, TabPanel, Tabs} from "react-tabs";
 import {ComicPage} from "./pages/ComicPage.jsx";
 import {HomePage} from "./pages/HomePage.jsx";
 import {useComicCollectionData} from "./api/comicInfo.js";
+import {Dropdown, Nav, Navbar, Container} from "react-bootstrap";
+import {useState} from "react";
 
-function App() {
-    const {comics, loading, error} = useComicCollectionData();
+const NAV_HOME = "NAV_HOME";
+const NAV_COMIC_SHELF = "NAV_COMIC_SHELF";
+const NAV_USERS = "NAV_USERS";
+
+function NavigationBar(props){
+    const {activeNavBarItem, onSelectNavBarItem} = props;
+
     return (
         <>
-            <Tabs>
-                <TabList>
-                    <Tab>
-                        Home
-                    </Tab>
-                    <Tab>
-                        Comic shelf
-                    </Tab>
-                </TabList>
+            <Navbar expand="md" bg="dark" className="fixed-top" data-bs-theme="dark">
+                <Container className="m-0">
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav activeKey={activeNavBarItem}
+                             onSelect={selectedEventKey => onSelectNavBarItem(selectedEventKey)}>
+                            <Nav.Item>
+                                <Nav.Link eventKey={NAV_HOME}>Home</Nav.Link>
+                            </Nav.Item>
 
-                <TabPanel>
-                    <HomePage comics={comics || []}/>
-                </TabPanel>
-                <TabPanel>
-                    <ComicPage comics={comics || []}/>
-                </TabPanel>
-            </Tabs>
+                            <Nav.Item>
+                                <Nav.Link eventKey={NAV_COMIC_SHELF}>Shelf</Nav.Link>
+                            </Nav.Item>
+
+                            <Dropdown>
+                                <Dropdown.Toggle variant="dark">UserList</Dropdown.Toggle>
+                                <Dropdown.Menu>
+                                    <Dropdown.Item eventKey={NAV_USERS}>User1</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+}
+
+function ActivePage(props){
+    const {activeNavBarItem} = props;
+    const {comics, loading, error} = useComicCollectionData();
+
+    switch (activeNavBarItem) {
+        case NAV_HOME:
+            return <HomePage comics={comics || []}/>;
+        case NAV_COMIC_SHELF:
+            return <ComicPage comics={comics}/>;
+        default:
+            return;
+    }
+}
+
+function App() {
+    const [activeNavBarItem, setActiveNavBarItem] = useState(NAV_HOME);
+
+    return (
+        <>
+            <NavigationBar activeNavBarItem={activeNavBarItem}
+                      onSelectNavBarItem={setActiveNavBarItem}/>
+            <div style={{marginTop: "70px"}}>
+                <ActivePage activeNavBarItem={activeNavBarItem}/>
+            </div>
         </>
     )
 }
