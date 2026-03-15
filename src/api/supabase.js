b6ca8b27c7523867d3a2a8a2687898cb1be4405c
StephanColson <151721516+StephanColson@users.noreply.github.com>
2026-03-15 14:43:58 +0100

    Removed Supabase

--- src/api/supabase.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseConfig = {
-    url: import.meta.env.VITE_SUPABASE_PROJECT_URL,
-    key: import.meta.env.VITE_SUPABASE_API_KEY,
-};
-
-export const supabase = createClient(supabaseConfig.url, supabaseConfig.key);
-console.log("Initialized Supabase connection");
